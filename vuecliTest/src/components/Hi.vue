3ff80a7b6135becff7a4e31aa6f7607715df8d64
LuLisong0411 <951170572@qq.com>
2018-12-25 21:45:05 +0800

    admin

---
+++ vuecliTest/src/components/Hi.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h2>Welcome to My Vue.</h2>    
+    </div>    
+</template>
+
+<script>
+export default {
+    name:'hi',
+    data:function(){
+        return {
+            msg: 'Welcome to My Vue.'
+        }
+    }
+}
+</script>
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
+
+
