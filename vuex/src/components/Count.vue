af16c080a523fe1a75bc43c728a87c3cc6a06826
LuLisong0411 <951170572@qq.com>
2019-04-30 20:28:25 +0800

    admin

---
+++ vuex/src/components/Count.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2>{{msg}}</h2>
+        <h2>{{$store.state.count}}</h2>
+        <p>
+            <button @click="$store.commit('add')">+</button>
+            <button @click="$store.commit('reduce')">-</button>
+        </p>
+    </div>
+    
+</template>
+
+<script>
+import store from '@/vuex/store';
+export default {
+    data() {
+        return {
+            msg:'Hello Vuex'
+        }
+    },
+    store
+}
+</script>
